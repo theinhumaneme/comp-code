c513a85faad2a63baa3d9c00be30b251fd0b5124
theinhumaneme <theinhumaneme@gmail.com>
2021-11-01 15:03:11 +0530

    updated gitignore

---
+++ rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    scanf("%d",&size);
+    int array[size];
+    for (int i =0;i<size;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    int rev_array[size];
+    for (int i =0;i<size;i++)
+    {
+        rev_array[i]=array[size-1-i];
+    }
+    for (int i =0;i<size;i++)
+    {
+        printf("%d",rev_array[i]);
+    }
+    
+}
